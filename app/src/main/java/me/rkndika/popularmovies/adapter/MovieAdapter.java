8265a3bc0a7ad19d84264bf17d5d64234bbce147
rkndika <rkndika@gmail.com>
2017-06-30 04:00:26 +0700

    Project Data

---
+++ app/src/main/java/me/rkndika/popularmovies/adapter/MovieAdapter.java
@@ -0,0 +1,85 @@
+package me.rkndika.popularmovies.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import me.rkndika.popularmovies.R;
+import me.rkndika.popularmovies.model.Movie;
+import me.rkndika.popularmovies.network.Config;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieAdapterViewHolder> {
+
+    private ArrayList<Movie> movies;
+    private Context context;
+
+    private final MovieAdapterOnClickHandler mClickHandler;
+
+    public interface MovieAdapterOnClickHandler {
+        void onItemClick(Movie movieClicked);
+    }
+
+    public MovieAdapter(MovieAdapterOnClickHandler clickHandler) {
+        mClickHandler = clickHandler;
+    }
+
+    public class MovieAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public final ImageView mPosterImageView;
+
+        public MovieAdapterViewHolder(View view) {
+            super(view);
+            mPosterImageView = (ImageView) view.findViewById(R.id.iv_poster_item);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int adapterPosition = getAdapterPosition();
+            Movie movieClicked = movies.get(adapterPosition);
+            mClickHandler.onItemClick(movieClicked);
+        }
+    }
+
+    @Override
+    public MovieAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.item_movie;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        return new MovieAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieAdapterViewHolder movieAdapterViewHolder, int position) {
+        Movie movie = movies.get(position);
+        Picasso.with(context)
+                .load(Config.IMAGE_URL + movie.getPosterPath())
+                .placeholder(R.drawable.rec_grey)
+                .error(R.drawable.rec_grey)
+                .into(movieAdapterViewHolder.mPosterImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (null == movies) return 0;
+        return movies.size();
+    }
+
+    public void setMoviesData(ArrayList<Movie> moviesData) {
+        movies = moviesData;
+        notifyDataSetChanged();
+    }
+
+    public ArrayList<Movie> getMoviesData() {
+        return movies;
+    }
+}
